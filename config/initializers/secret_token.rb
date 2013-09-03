ced0efa88fad44032017959fbded72ee255f7905
Neilos <nosniktatkinson@hotmail.co.uk>
2013-09-03 08:56:49 +0100

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetingRooms::Application.config.secret_key_base = '02f424bb54a6b5e2984d542a7f1b4e528f7600a165ef008393707dc61a92d5c58f06157534d7f465186fb31f17c3638e542021b6eec5b1c2ed5014fb06b85bd7'
