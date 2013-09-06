25d42878996fd014d52bb9a5d3a24d2f86f61831
Neilos <nosniktatkinson@hotmail.co.uk>
2013-09-06 09:24:30 +0100

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.find_or_create_by_email :name => 'first person', :email => 'user1@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'user: ' << user1.name
+
+user2 = User.find_or_create_by_email :name => 'second person', :email => 'user2@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'user: ' << user2.name
+
+user3 = User.find_or_create_by_email :name => 'third person', :email => 'user3@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'user: ' << user3.name
