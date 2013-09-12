b1c6f70a5a12ecbac88c02fd819f382afcf80b0d
Mentessi <mentessi@gmail.com>
2013-09-12 15:55:25 +0100

    user can view room

--- spec/features/room/viewing_room_feature_spec.rb
+++ spec/features/room/viewing_room_feature_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 feature "viewing room" do
-	scenario ""
+	before :each do 
+		@password = "password"
+		@user = FactoryGirl.create(:user, email: "user1@email.com", password: @password, password_confirmation: @password)
+		@organization = Organization.create(:name => "techhub")
+		@location = FactoryGirl.create(:location, organization_id: @organization.id, name: "Old Street")
+		@room = FactoryGirl.create(:room, organization_id: @organization.id, name: "Room 1", location_id: @location.id)
+		sign_in_with email: @user.email, password: @password
+	end
+
+	scenario "via the organization#show page" do
+		within(".navbar") do
+			click_link('Organizations')
+		end
+		within("#organizations-table") do 
+			page.first(:link, 'View').click
+		end
+		within("#rooms-table") do 
+			page.first(:link, 'View').click
+		end
+		expect(page).to have_content @room.price_per_hour
+		expect(page).to have_content @room.people_capacity
+		expect(page).to have_content @room.name
+		click_link 'Back'
+		expect(page).to have_content 'techhub'
+  	expect(page).to have_selector '#organization-details'
+ 	 	expect(page).to have_selector '#rooms-table'
+ 		within('#rooms-table') do
+ 	  	page.should have_content(@room.name)
+  	end
+	end
 end
