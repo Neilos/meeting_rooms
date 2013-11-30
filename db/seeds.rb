751a92d862540cfa1e9919d1b8c3bddb92b7db1d
Neilos <nosniktatkinson@hotmail.co.uk>
2013-11-30 14:34:19 +0000

    Amend sede file to include bookings.

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,3 +40,10 @@ puts 'room: ' << room1.name
 
 room2 = Room.where(name: "Room 2.01", price_per_hour: "60.00", location_id: location2.id, organization_id: organization1.id, people_capacity: 1).first_or_create
 puts 'room: ' << room2.name
+
+
+booking1 = Booking.where(name: "Booking1", description: "Booking 1 description", is_all_day: false, from_date: "2013-11-29", from_time: "2013-11-29 11:17:07", to_date: "2013-11-29", to_time: "2013-11-29 11:17:07", repeats: "MyString", repeats_every_n_days: 1, repeats_every_n_weeks: 0, repeats_weekly_each_days_of_the_week_mask: 0, repeats_every_n_months: 0, repeats_monthly: "MyString", repeats_monthly_each_days_of_the_month_mask: 0, repeats_monthly_on_ordinals_mask: 0, repeats_monthly_on_days_of_the_week_mask: 0, repeats_every_n_years: 0, repeats_yearly_each_months_of_the_year_mask: 0, repeats_yearly_on: false, repeats_yearly_on_ordinals_mask: 0, repeats_yearly_on_days_of_the_week_mask: 0, repeat_ends: "MyString", repeat_ends_on: "2014-11-29", time_zone: "MyString", room_id: room1.id, user_id: user1.id).first_or_create
+puts 'booking: ' << booking1.name
+
+booking2 = Booking.where(name: "Booking1", description: "Booking 1 description", is_all_day: false, from_date: "2013-11-29", from_time: "2013-11-29 11:17:07", to_date: "2013-11-29", to_time: "2013-11-29 11:17:07", repeats: "MyString", repeats_every_n_days: 0, repeats_every_n_weeks: 1, repeats_weekly_each_days_of_the_week_mask: 0, repeats_every_n_months: 0, repeats_monthly: "MyString", repeats_monthly_each_days_of_the_month_mask: 0, repeats_monthly_on_ordinals_mask: 0, repeats_monthly_on_days_of_the_week_mask: 0, repeats_every_n_years: 0, repeats_yearly_each_months_of_the_year_mask: 0, repeats_yearly_on: false, repeats_yearly_on_ordinals_mask: 0, repeats_yearly_on_days_of_the_week_mask: 0, repeat_ends: "MyString", repeat_ends_on: "2014-11-29", time_zone: "MyString", room_id: room2.id, user_id: user2.id).first_or_create
+puts 'booking: ' << booking2.name
