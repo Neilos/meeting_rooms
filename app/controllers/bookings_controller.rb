7dc314582d24d80e2ab45f1d4895b37a039ce790
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-01 15:50:29 +0000

    Adjust indentation.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -65,14 +65,14 @@ class BookingsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def booking_params
-      params.require(:booking).permit(:name, :description, :is_all_day, :from_date, :from_time, :to_date, :to_time, :repeats, :repeats_every_n_days, :repeats_every_n_weeks, :repeats_weekly_each_days_of_the_week_mask, :repeats_every_n_months, :repeats_monthly, :repeats_monthly_each_days_of_the_month_mask, :repeats_monthly_on_ordinals_mask, :repeats_monthly_on_days_of_the_week_mask, :repeats_every_n_years, :repeats_yearly_each_months_of_the_year_mask, :repeats_yearly_on, :repeats_yearly_on_ordinals_mask, :repeats_yearly_on_days_of_the_week_mask, :repeat_ends, :repeat_ends_on, :time_zone, :room_id, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def booking_params
+    params.require(:booking).permit(:name, :description, :is_all_day, :from_date, :from_time, :to_date, :to_time, :repeats, :repeats_every_n_days, :repeats_every_n_weeks, :repeats_weekly_each_days_of_the_week_mask, :repeats_every_n_months, :repeats_monthly, :repeats_monthly_each_days_of_the_month_mask, :repeats_monthly_on_ordinals_mask, :repeats_monthly_on_days_of_the_week_mask, :repeats_every_n_years, :repeats_yearly_each_months_of_the_year_mask, :repeats_yearly_on, :repeats_yearly_on_ordinals_mask, :repeats_yearly_on_days_of_the_week_mask, :repeat_ends, :repeat_ends_on, :time_zone, :room_id, :user_id)
+  end
 end
