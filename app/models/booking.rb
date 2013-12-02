df999f5b2918e0c04c41bfa470659d038f81dbba
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-02 10:35:53 +0000

    Add extra validations to Booking model.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,19 @@
+require 'booking_methods'
 class Booking < ActiveRecord::Base
+  include BookingMethods
   belongs_to :room, :inverse_of => :bookings
   belongs_to :user, :inverse_of => :bookings
+
+  validates_presence_of :name
+  validates_presence_of :room_id
+  validates_presence_of :user_id
+  validates_presence_of :from_date
+  validates_presence_of :to_date
+  validates_presence_of :time_zone
+  validates_presence_of :from_time, :if => :not_all_day?
+  validates_presence_of :to_time, :if => :not_all_day?
+
+  def not_all_day?
+    !is_all_day
+  end
 end
