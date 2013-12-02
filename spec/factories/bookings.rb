b64bfe2f51d6dd943c4627dba9b2da95df7838ee
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-02 10:36:13 +0000

    Amend Booking factory to comply with new validations.

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -9,23 +9,8 @@ FactoryGirl.define do
     sequence :from_time do |n| (n + 1).days.ago end
     sequence :to_date do |n| (n).days.ago end
     sequence :to_time do |n| (n).days.ago end
-    repeats "repeats"
-    repeats_every_n_days 1
-    repeats_every_n_weeks 1
-    repeats_weekly_each_days_of_the_week_mask 1
-    repeats_every_n_months 1
-    repeats_monthly "repeats monthly"
-    repeats_monthly_each_days_of_the_month_mask 1
-    repeats_monthly_on_ordinals_mask 1
-    repeats_monthly_on_days_of_the_week_mask 1
-    repeats_every_n_years 1
-    repeats_yearly_each_months_of_the_year_mask 1
-    repeats_yearly_on false
-    repeats_yearly_on_ordinals_mask 1
-    repeats_yearly_on_days_of_the_week_mask 1
-    repeat_ends "repeat ends"
-    repeat_ends_on { 1.year.from_now }
-    time_zone "MyString"
+    repeats 'never'
+    time_zone "London"
     association :room, factory: :room
     association :user, factory: :user
   end
