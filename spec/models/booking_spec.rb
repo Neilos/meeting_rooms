5b76a72dc05b754df7edf09b3eaf918ce33b23c5
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-05 16:53:20 +0000

    Test validation of presence of repeats.

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe Booking do
 
   let(:attributes) { FactoryGirl.build(:booking).attributes }
-
-  [ "name", "calendar_id", "booker_id", "time_zone" ].each do |attribute|
+  [ "name", "calendar_id", "booker_id", "time_zone", "repeats" ].each do |attribute|
     it "should not be valid without #{attribute}" do
       attributes.delete(attribute)
       Booking.create(attributes).should_not be_valid
