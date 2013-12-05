1bd92a2775be34ce38cc73531a6fa98bdfbdb545
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-05 13:16:22 +0000

    Some booking attributes are set by default.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,16 +4,20 @@ class Booking < ActiveRecord::Base
   belongs_to :calendar, :inverse_of => :bookings
   delegate :room, :to => :calendar, :allow_nil => true
   belongs_to :booker, :class_name => "User", foreign_key: "booker_id", :inverse_of => :bookings
+  before_validation :set_default_values_for_blank_fields
   validates_presence_of :name
   validates_presence_of :calendar_id
   validates_presence_of :booker_id
-  validates_presence_of :from_date
-  validates_presence_of :to_date
   validates_presence_of :time_zone
-  validates_presence_of :from_time, :if => :not_all_day?
-  validates_presence_of :to_time, :if => :not_all_day?
 
-  def not_all_day?
-    !is_all_day
+
+  def set_default_values_for_blank_fields
+    self.from_date = Date.today unless from_date
+    self.to_date = Date.today unless to_date
+    unless is_all_day
+      self.from_time = Time.now.beginning_of_day unless from_time
+      self.to_time = Time.now.end_of_day unless to_time
+    end
   end
+
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -10,5 +10,19 @@ describe Booking do
       Booking.create(attributes).should_not be_valid
     end
   end
-  
+
+  context "with missing attributes that are set by default" do
+    let(:default_attrs){ ["from_date", "to_date", "from_time", "to_time"] }
+    let(:default_attrs_missing) { attributes.select{|attribute| !default_attrs.include?(attribute)} }
+    subject { Booking.create(default_attrs_missing) }
+    specify { should be_valid }
+    its(:from_date) { should == Date.today }
+    its(:to_date) { should == Date.today }
+
+    context "when all day" do
+      its(:from_time) { should == Time.now.beginning_of_day }
+      its(:to_time) { should == Time.now.end_of_day }
+    end
+  end
+
 end
