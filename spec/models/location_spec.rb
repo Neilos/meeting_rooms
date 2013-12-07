004ca6f241eacc3176fd668c5de7d94700fbf9c3
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-07 12:00:33 +0000

    Location model specs refactored.

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 describe Location do
 
-  @attributes = {name: "building", address_line_1: "the street", address_line_2: "more road", town_city: "london", county: "essex", postcode: "RM9 5YT", country: "UK", organization_id: 1}
-	
-	@attributes.each do |attribute, value|
-		it "should not be valid without #{attribute}" do
-			missing_attributes = {name: "building", address_line_1: "the street", address_line_2: "more road", town_city: "london", county: "essex", postcode: "RM9 5YT", country: "UK", organization_id: 1}
-			missing_attributes.delete(attribute)
-			Location.create(missing_attributes).should_not be_valid
-		end
-	end
+  let(:attributes) { FactoryGirl.build(:location).attributes }
+
+  @essential_attrs = [ "name", "address_line_1", "address_line_2", "town_city", "county", "postcode", "country", "organization_id" ]
+  @essential_attrs.each do |attribute|
+    it "should not be valid without #{attribute}" do
+      with_missing_attribute = attributes
+      with_missing_attribute.delete(attribute)
+      Location.create(with_missing_attribute).should_not be_valid
+    end
+  end
 
   it "should be valid with all fields completed" do 
-  	Location.create(name: "building", address_line_1: "the street", address_line_2: "more road", town_city: "london", county: "essex", postcode: "RM9 5YT", country: "UK", organization_id: 1).should be_valid
+  	Location.create(attributes).should be_valid
   end
 
   it "should have a name that is unique within it's organization" do 
-    attributes = {name: "building", address_line_1: "the street", address_line_2: "more road", town_city: "london", county: "essex", postcode: "RM9 5YT", country: "UK", organization_id: 1}
     Location.create(attributes).should be_valid
     Location.create(attributes).should_not be_valid
     Location.create(attributes.merge({organization_id: 2})).should be_valid
