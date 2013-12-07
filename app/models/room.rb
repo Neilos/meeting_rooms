4feae1736f4f1b638d3169febe85422ca9ac0c20
Neilos <nosniktatkinson@hotmail.co.uk>
2013-12-07 13:06:43 +0000

    Autosave and validate room association models upon save

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,12 @@
 class Room < ActiveRecord::Base
 	belongs_to :organization, :inverse_of => :rooms
 	belongs_to :location, :inverse_of => :rooms
-	has_many :custom_attributes, :dependent => :destroy, :inverse_of => :room
-  has_one :calendar, :dependent => :destroy, :inverse_of => :room
+	has_many :custom_attributes, :dependent => :destroy, :inverse_of => :room, :autosave => true
+  has_one :calendar, :dependent => :destroy, :inverse_of => :room, :autosave => true
   has_many :bookings, :through => :calendar
 
   validates_associated :custom_attributes
+  validates_associated :calendar
   validates :name, :location_id, :organization_id, presence: :true
   validates_uniqueness_of :name, :scope => :location_id
 
