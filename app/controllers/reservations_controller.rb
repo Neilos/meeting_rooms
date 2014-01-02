7f2d2e2ec16d368a9759a920c68630129aa30a3b
Neilos <nosniktatkinson@hotmail.co.uk>
2014-01-02 17:39:11 +0000

    Add a index action to reservations controller.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,8 @@
 class ReservationsController < ApplicationController
   def index
+    begin_date = DateTime.parse(params[:begin_date])
+    end_date = DateTime.parse(params[:end_date])
+    calendar_ids = params[:calendar_ids]
+    @reservations = Reservation.occurrences_between(begin_date, end_date, calendar_ids)
   end
 end
--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -3,10 +3,28 @@ require 'spec_helper'
 describe ReservationsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
+
+    let!(:repeating_booking) { FactoryGirl.create(:booking,
+      from_date: DateTime.now.beginning_of_day,
+      from_time: DateTime.now.beginning_of_day,
+      to_date: DateTime.now.beginning_of_day,
+      to_time: DateTime.now.beginning_of_day + 1.hour,
+      repeats: "daily",
+      repeats_every_n_days: 1,
+      repeat_ends: 'never')
+    }
+
+    it "returns the correct number of reservations for the specified period" do
+      params = {
+        begin_date: DateTime.now.beginning_of_day,
+        end_date: DateTime.now.beginning_of_day + 5.days - 1.second,
+        calendar_ids: repeating_booking.calendar.id.to_s,
+      }
+      get 'index', params
       response.should be_success
+      expect(assigns(:reservations).count).to eq 5
     end
+
   end
 
 end
